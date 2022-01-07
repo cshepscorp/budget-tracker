e3aa14c0b0cb6f06d9deb54ba46c6a68b83a5a2e
cshepscorp <sheppard.christy@gmail.com>
2022-01-07 09:51:22 -0500

    minor change to manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,18 +6,19 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 // which items are we going to cache?
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./js/idb.js",
-    "./js/index.js",
-    "./css/styles.css",
-    "./icons/icon-512x512.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-72x72.png"
+    "/",
+    "/index.html",
+    "/js/idb.js",
+    "/js/index.js",
+    "/css/styles.css",
+    "/icons/icon-512x512.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-72x72.png"
   ];
 
 self.addEventListener('install', function (e) {
